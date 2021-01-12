94d4fd5f2477c3eed031e5eca158636cb003667e
devggaurav <devggaurav@gmail.com>
2021-01-12 15:28:13 +0530

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-center">
+      <nuxt-link to="/">Home</nuxt-link>
+      <nuxt-link to="blog">Blog</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-center">
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+    export default {
+      head() {
+        return {
+          script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+        };
+      },
+    };
+</script>
+
+<style>
+
+</style>
